3bd49368b0fa485b245dcc9376ad62c7e68f353b
FloopyG <36450035+FloopyG@users.noreply.github.com>
2020-11-15 18:43:59 +0200

    Initial commit

---
+++ Lab6.1(iter).cpp
@@ -0,0 +1,64 @@
+﻿#include <iostream>
+#include <time.h>
+#include <iomanip>
+
+using namespace std;
+
+void Create(int* c, const int size, int min, int max) {
+	for (int i = 0; i < size; i++) {
+		c[i] = min + rand() % (max - min + 1);
+	}
+}
+
+void arrPrint(int* c, const int size) {
+	for (int i = 0; i < size; i++) {
+		cout << setw(4) << c[i];
+	}
+		cout << endl;
+}
+
+// c[i] % 2 == 0 || i % 8 == 0
+
+int arrSum(int* c, const int size, int S) {
+	for (int i = 0; i < size; i++) {
+		if (c[i] % 2 == 0 || (i + 1) % 8 != 0) {
+			S += c[i];
+			c[i] = 0;
+		}
+	}
+	return S;
+}
+
+int arrCount(int* c, const int size, int z) {
+	for (int i = 0; i < size; i++) {
+		if (c[i] == 0) {
+			z++;
+		}
+	}
+	return z;
+}
+
+
+int main()
+{
+	srand((unsigned)time(NULL));
+
+	const int a = 25;
+
+	int c[a];
+
+	int Low = 5;
+	int High = 90;
+
+	Create(c, a, Low, High);
+	arrPrint(c, a);
+	cout << "S = " << arrSum(c, a, 0) << endl;
+	cout << "c[i] = 0 count: " << arrCount(c, a, 0) << endl;
+	arrPrint(c, a);
+
+
+
+	system("pause");
+	cin.get();
+	return 0;
+}
---
+++ Lab6.1(iter)Test/Lab6.1(iter)Test.cpp
@@ -0,0 +1,22 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab6.1(iter).cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace Lab61iterTest
+{
+	TEST_CLASS(Lab61iterTest)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			int t;
+			const int a = 4;
+			int c[a] = { 1, 1, 1, 1 };
+			t = arrSum(c, a, 0);
+			Assert::AreEqual(t, 4);
+		}
+	};
+}
